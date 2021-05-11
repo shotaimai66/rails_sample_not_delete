aec266e07811125f52791c3aec0d3c0e61021578
FireEnginner <shota.act.6@gmail.com>
2021-05-11 23:25:10 +0900

    add Docker

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,10 +1,3 @@
 const { environment } = require('@rails/webpacker')
 
-const webpack = require('webpack')
-environment.plugins.append('Provide', new webpack.ProvidePlugin({
-    $: 'admin-lte/plugins/jquery/jquery',
-    jQuery: 'admin-lte/plugins/jquery/jquery',
-    Popper: ['popper.js', 'default']
-}))
-
 module.exports = environment
