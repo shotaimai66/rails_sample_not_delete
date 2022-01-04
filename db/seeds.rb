6c6c9da4ad4e0e98c108ccede7be963c9664c88f
shotaimai66 <changemymind6@gmail.com>
2022-01-05 07:52:20 +0900

    fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@
 
 50.times do |i|
   user = User.new(
-    email: "test_user#{i}@gmail.com",
+    email: "test_user#{i}@gmail.com", # sample: test_user1@gmail.com
     name: "テストuser#{i}",
     password: 'password'
   )
