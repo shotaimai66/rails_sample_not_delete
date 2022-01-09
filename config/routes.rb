e62fa01df7f924cc557cc182dc6deca1c485931b
shotaimai66 <changemymind6@gmail.com>
2022-01-09 16:39:00 +0900

    add shell

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-Rails.application.routes.draw do # rubocop:disable Metrics/BlockLength
+Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 
   devise_scope :user do
---
+++ docker/scripts/test.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#
+# アプリケーションのテストを実行する
+# @note 引数はRSpec実行時のオプションとしてデリゲートされる
+#
+set -e;
+cd /webapp;
+
+# 環境変数を設定
+export RAILS_ENV="test";
+
+# テストの実行
+bundle exec rspec $*;
+bundle exec rubocop;
+
+# erdの生成
+bundle exec erd;
+
+# テストの終了
+echo "\e[32m";
+echo "\n  =========================\n";
+echo "         TEST PASSED";
+echo "\n  =========================\n";
+echo "\e[m";
+exit 0;
