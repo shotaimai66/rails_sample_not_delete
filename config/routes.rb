1a8dfc5175ab8d6b3d788378388776a78c1f6c04
shotaimai66 <changemymind6@gmail.com>
2022-01-10 10:27:12 +0900

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,15 @@
 Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 
+# admin関連=========================================================
+  devise_for :admins, controllers: {
+    sessions: 'admins/sessions'
+  }
+
+# =================================================================
+
+
+# user関連==========================================================
   devise_scope :user do
     root 'users/sessions#new'
   end
@@ -14,22 +23,26 @@ Rails.application.routes.draw do
     registrations: 'users/registrations'
   }
 
-  devise_for :admins, controllers: {
-    sessions: 'admins/sessions'
-  }
-  devise_for :managers, controllers: {
-    sessions:      'managers/sessions',
-    passwords:     'managers/passwords',
-    confirmations: 'users/confirmations',
-    registrations: 'managers/registrations'
-  }
-
   namespace :users do
     resources :dash_boards, only: [:index]
     resources :articles, only: %i[index show]
     resource :profile, except: %i[create new]
   end
 
+# =================================================================
+
+# manager関連=======================================================
+  devise_for :managers, controllers: {
+    sessions:      'managers/sessions',
+    passwords:     'managers/passwords',
+    confirmations: 'users/confirmations',
+    registrations: 'managers/registrations'
+  }
+# =================================================================
+
+
+# 共通==============================================================
   # 利用規約
   get 'use' => 'use#index'
+# =================================================================
 end
